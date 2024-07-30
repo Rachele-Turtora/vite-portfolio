963bddf31c131e9be027b19e3189d26d74cd9137
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-30 16:30:53 +0200

    [UPD] getProjects() axios (milestone 4)

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,30 @@
 import axios from 'axios';
 
 export default {
-  name: "MyApp"
+  name: "MyApp",
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    getProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then(response => {
+        this.projects = response.data.results;
+      })
+    }
+  },
+  created() {
+    this.getProjects();
+  }
 }
 </script>
 
 <template>
   <h1>MyApp</h1>
+  <ul>
+    <li v-for="project in projects">{{ project['title'] }}</li>
+  </ul>
 </template>
 
 <style scoped></style>
